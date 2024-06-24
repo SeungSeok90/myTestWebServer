4780bc93dfcede44a5a45b638d160343db5e65c0
Seungseok Shin <shinss305@gmail.com>
2024-06-24 13:14:31 +0900

    port 3000 변경

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-const port = 80;
+const port = 3000;
 
 // 정적 파일 제공
 app.use(express.static(path.join(__dirname, 'public')));
